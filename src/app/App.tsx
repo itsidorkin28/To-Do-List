6295a11abeacff91346946ef14c58c6eff38967e
itsidorkin28 <itsidorkin28@gmail.com>
2022-01-02 16:05:26 +0300

    added HashRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -60,6 +60,7 @@ export const App = React.memo(({demo = false}: AppPropsType) => {
             <Container fixed>
                 <Routes>
                     <Route path={'/'} element={<TodolistsList demo={demo}/>}/>
+                    <Route path={'/to-do-list'} element={<TodolistsList demo={demo}/>}/>
                     <Route path={'/login'} element={<Login/>}/>
                     <Route path={'/404'} element={<h1 style={{textAlign: 'center'}}>404: PAGE NOT FOUND</h1>}/>
                     <Route path={'*'} element={<Navigate to={'/404'}/>}/>
